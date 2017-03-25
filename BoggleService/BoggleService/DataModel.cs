a0a2068f6563904ec3f0aa5706ea55c33bad2626
kydia4ever <u0040235@utah.edu>
2017-03-24 19:27:57 -0600

    save

--- BoggleService/BoggleService/DataModel.cs
+++ BoggleService/BoggleService/DataModel.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Boggle
+namespace BoggleList
+
 {
-    public class DataModel
-    {
 
         public class UserInfo
         {
             public string Name { get; set; }
 
         }
-    }
+ 
 }
