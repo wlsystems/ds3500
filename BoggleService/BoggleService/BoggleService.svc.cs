b674b74dc4af5b87d99313129118ea4e60912c65
kydia4ever <u0040235@utah.edu>
2017-03-26 18:51:15 -0600

    took out duplicate

--- BoggleService/BoggleService/BoggleService.svc.cs
+++ BoggleService/BoggleService/BoggleService.svc.cs
@@ -43,7 +43,6 @@ namespace Boggle
                 }
                 else
                 {
-                    SetStatus(Created);
                     string userID = Guid.NewGuid().ToString();
                     PlayerCompleted user = new PlayerCompleted();
                     user.Nickname = newUser.Nickname;
